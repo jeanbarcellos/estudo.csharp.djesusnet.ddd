877cd4136ac1ba621aebd59fd8b20471b666ad24
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-10 16:30:25 -0300

    Infrastructure - Criação dos Projetos Data e CrossCurring

---
+++ src/WebApi.Infrastructure.Data/Contexts/SqlContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Infrastructure.Data.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public SqlContext()
+        {
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreatedAt") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("CreatedAt").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("CreatedAt").IsModified = false;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+    }
+}
---
+++ src/WebApi.Infrastructure.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Domain.Core.Interfaces.Repositories;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Infrastructure.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly DbContext _context;
+
+        public BaseRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public void Add(TEntity obj)
+        {
+            try
+            {
+                _context.Set<TEntity>().Add(obj);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(TEntity obj)
+        {
+            try
+            {
+                _context.Entry(obj).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Remove(TEntity obj)
+        {
+            try
+            {
+                _context.Set<TEntity>().Remove(obj);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+
+}
