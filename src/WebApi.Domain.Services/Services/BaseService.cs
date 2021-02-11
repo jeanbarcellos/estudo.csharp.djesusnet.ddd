1202be90a18393c623e48afd5c26157dec3d760e
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-11 11:58:25 -0300

    Remoção de using desnecessários

--- src/WebApi.Domain.Services/Services/BaseService.cs
+++ src/WebApi.Domain.Services/Services/BaseService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using WebApi.Domain.Core.Interfaces.Repositories;
 using WebApi.Domain.Core.Interfaces.Services;
 using WebApi.Domain.Entities;
