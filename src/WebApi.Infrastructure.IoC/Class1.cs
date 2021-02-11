ef33bb6d1c7729322cf9531072dc352a3bb2d52b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-10 23:46:37 -0300

    Infrastructure - Implementação do IoC

--- src/WebApi.Infrastructure.IoC/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WebApi.Infrastructure.IoC
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/WebApi.Infrastructure.IoC/ConfigurationIoC.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using AutoMapper;
+using WebApi.Application.Interfaces;
+using WebApi.Application.Services;
+using WebApi.Application.Mappers;
+using WebApi.Domain.Core.Interfaces.Repositories;
+using WebApi.Domain.Core.Interfaces.Services;
+using WebApi.Domain.Services.Services;
+using WebApi.Infrastructure.Data;
+
+namespace WebApi.Infrastructure.IoC
+{
+    public class ConfigurationIoC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IoC
+
+            builder.RegisterType<ClientApplicationService>().As<IClientApplicationService>();
+            builder.RegisterType<ProductApplicationService>().As<IProductApplicationService>();
+
+            builder.RegisterType<ClientService>().As<IClientService>();
+            builder.RegisterType<ProductService>().As<IProductService>();
+
+            builder.RegisterType<ClientRepository>().As<IClientRepository>();
+            builder.RegisterType<ProductRepository>().As<IProductRepository>();
+
+            builder.Register(ctx => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ClientDTOToEntityMapping());
+                cfg.AddProfile(new ClientEntityToDTOMapping());
+                cfg.AddProfile(new ProductDTOToEntityMapping());
+                cfg.AddProfile(new ProductEntityToDTOMapping());
+
+            }));
+
+            #endregion IoC
+        }
+
+    }
+}
